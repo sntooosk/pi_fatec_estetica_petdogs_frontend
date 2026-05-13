7b28a351a4aed5e0042a134bca8aad05c1731a80
jlmed55 <jlgamerealviolino@gmail.com>
2026-05-13 20:19:57 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className='flex items-center'>
+        <h1 className='text=3xl font-bold'>
+          Hello vite +react </h1>
+        </div>
+        
+  )
+}
+
+export default App
