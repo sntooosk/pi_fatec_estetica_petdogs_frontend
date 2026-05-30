704e2c94285ac6616abec5719815dc3f87ccb4ec
Juliano Cassimiro Dos Santos <Juliano.cassimiro@qualitydigital.global>
2026-05-29 21:55:04 -0300

    refactor: padroniza nomenclaturas e remove arquivos obsoletos

--- src/pages/files/FilesPages.tsx
+++
@@ -1,3 +0,0 @@
-export function FilesPages() {
-  return <h1 className="text-3xl font-bold">Arquivos</h1>
-}
--- src/services/authService.ts
+++
@@ -1,9 +0,0 @@
-import type { AuthSession, LoginCredentials } from "../domain/entities"
-import { authController } from "../interface-adapters/controllers/authController"
-
-export type LoginData = LoginCredentials
-export type LoginResponse = AuthSession
-
-export function LoginRequest(data: LoginData): Promise<LoginResponse> {
-  return authController.signIn(data)
-}
